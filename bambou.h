65ed3c028ba0a586ea06cd110c2f6469a9a874f4
nicolas.poirier91@orange.fr <nicolas.poirier91@orange.fr@2574f1c7-e793-a350-a901-18184fd330ae>
2010-06-21 13:49:57 +0000

    - Texture du heightmap - Placement des bambous rapide

--- bambou.h
+++ bambou.h
@@ -2,9 +2,10 @@
 #define __bambou_h__
 
 #include "common.h"
+#include "heightmap.h"
 
 void faire_composantes_bambou();
 void faire_bambou(GLuint liste, int hauteur, int nb_branches);
-int faire_foret_bambous(int nb_bambous, int nb_varietes);
+int faire_foret_bambous(int nb_bambous, int nb_varietes, char* pHeightMap);
 
 #endif /* __bambou_h__ */
